9e0a572374d49b52d01c886e961d82380ba70038
josephriosio <blankworldwide@gmail.com>
2020-03-28 22:57:31 -0700

    more styling fixes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import './App.css';
 import ServersPanel from './components/ServersPanel';
 
 function App() {
+  const date = new Date();
   return (
     <div className='App'>
       <Nav />
       <ServersPanel />
+      <section className='discord-section'>
+        <a className='btn' href='https://discord.gg/FxNpvp6'>
+          <span class='socicon socicon-discord padding-right'></span>
+          Join
+        </a>
+      </section>
+
+      <p>© {date.getFullYear()} RustLab</p>
     </div>
   );
 }
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,7 +4,9 @@ import './Nav.css';
 const Nav = () => {
   return (
     <nav className='navbar'>
-      <h1 id='logo'>RustLab</h1>
+      <div className='nav-brand'>
+        <h1 id='logo'>RustLab</h1>
+      </div>
     </nav>
   );
 };
